8a5d18dbcadd6b81b2676a675ce3643bfcedaec4
Cong Dinh <congdinh2012@hotmail.com>
2025-03-10 22:27:25 +0700

    Server: Create Java Spring Boot Project

---
+++ src/server/src/main/java/com/congdinh/vivuchat/VivuchatApplication.java
@@ -0,0 +1,13 @@
+package com.congdinh.vivuchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivuchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivuchatApplication.class, args);
+	}
+
+}
